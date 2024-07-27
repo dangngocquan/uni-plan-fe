b29c6da1a7cf94febed24b9a9f1933967119aa51
dangngocquan <dangngocquan104@gmail.com>
2024-07-27 23:30:40 +0700

    feat: school page

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -1,10 +1,13 @@
+import classNames from "classnames";
 import MainLayout from "../layouts/MainLayout";
 import About from "./About";
 import HeroSection from "./HeroSection";
 
+import styles from "./index.module.scss";
+
 const LandingPage = () => {
   return (
-    <div>
+    <div className={classNames(styles.wrapper)}>
       <MainLayout>
         <HeroSection />
         <About/>
